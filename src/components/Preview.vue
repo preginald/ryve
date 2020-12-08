b20549c72a10b0ae475c5ad7270546798f2e1507
Peter Reginald <peter@digitalsanctum.com>
2020-12-08 22:00:01 +1100

    removed container from Preview.vue

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,57 +1,41 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">{{ heading }}</h2>
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">Code</th>
-                <th class="text-left">Type</th>
-                <th class="text-left">Item</th>
-                <th class="text-right">Qty</th>
-                <th class="text-right">$</th>
-                <th class="text-right">Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in items" :key="item.name">
-                <td>{{ item.code }}</td>
-                <td>
-                  <v-chip
-                    small
-                    label
-                    text-color="white"
-                    :color="chipColour(item.type)"
-                    >{{ item.type }}</v-chip
-                  >
-                </td>
-                <td>{{ item.name }}</td>
-                <td class="text-right">{{ item.qty }}</td>
-                <td class="text-right">{{ item.price }}</td>
-                <td class="text-right">{{ item.lineTotal }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-col>
-    </v-row>
-    <v-row v-if="debug">
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Debug</h2>
-
-        <ul>
-          <li>TV size:{{ tvsize }}</li>
-          <li>Mount surface:{{ mount_surface }}</li>
-          <li>Mount type:{{ mount_type }}</li>
-          <li>Mount recessed:{{ mount_recessed }}</li>
-          <li>Conceal wires:{{ conceal_wires }}</li>
-          <li>Concealable cables:{{ concealableCables }}</li>
-        </ul>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row>
+    <v-col class="mb-5" cols="12">
+      <h2 class="headline font-weight-bold mb-3">{{ heading }}</h2>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Code</th>
+              <th class="text-left">Type</th>
+              <th class="text-left">Item</th>
+              <th class="text-right">Qty</th>
+              <th class="text-right">$</th>
+              <th class="text-right">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in items" :key="item.name">
+              <td>{{ item.code }}</td>
+              <td>
+                <v-chip
+                  small
+                  label
+                  text-color="white"
+                  :color="chipColour(item.type)"
+                  >{{ item.type }}</v-chip
+                >
+              </td>
+              <td>{{ item.name }}</td>
+              <td class="text-right">{{ item.qty }}</td>
+              <td class="text-right">{{ item.price }}</td>
+              <td class="text-right">{{ item.lineTotal }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -59,7 +43,7 @@ export default {
   name: "preview",
   props: ["items"],
   data: () => ({
-    heading: "Preview",
+    heading: "Live preview",
   }),
   methods: {
     chipColour(type) {
